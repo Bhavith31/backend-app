aea86c7a25a18f54c5a6eb698b4415d0e8af6aeb
SirikondaBhavith31 <sirikonda3107@gmail.com>
2026-03-09 15:26:00 +0530

    update

--- config/db.js
+++ config/db.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
+
 const dbConnect = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
+    console.log("Database connected successfully");
   } catch (err) {
-    console.log(err);
+    console.log("Database connection error:", err);
   }
 };
 export default dbConnect;
