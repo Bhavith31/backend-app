3ed6b92826f54fb2476ea973c0d6980f48df07c2
SirikondaBhavith31 <sirikonda3107@gmail.com>
2026-03-10 11:27:07 +0530

    update

--- config/db.js
+++ config/db.js
@@ -12,5 +12,42 @@ const dbConnect = async () => {
 
 export default dbConnect;
 
-
-
+// db.products.insertMany([
+//   {
+//     name: "Product 1",
+//     description: "Description for product 1",
+//     price: 199,
+//     image: "/images/1.PNG",
+//   },
+//   { name: "Product 2",    
+//     description: "Description for product 2",
+//     price: 299,
+//     image: "/images/2.PNG",
+//   },
+//   {
+//     name: "Product 3",
+//     description: "Description for product 3",
+//     price: 399,
+//     image: "/images/3.PNG",
+//   },
+// ]);
+// db.products.insertMany([
+//   {
+//     name: "Product 6",
+//     description: "Description for product 6",
+//     price: 699,
+//     image: "/images/6.PNG",
+//   },
+//   {
+//     name: "Product 7",
+//     description: "Description for product 7",
+//     price: 799,
+//     image: "/images/7.PNG",
+//   },
+//   {
+//     name: "Product 8",
+//     description: "Description for product 8",
+//     price: 899,
+//     image: "/images/8.PNG",
+//   }
+// ]);
