ad688f0224d69d4ac50ebb35ec5cb17907b4c791
SirikondaBhavith31 <sirikonda3107@gmail.com>
2026-03-10 11:49:05 +0530

    update

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import productModel from "../models/productModel.js";
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URl);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Database connected successfully");
   } catch (err) {
     console.log("Database connection error:", err);
